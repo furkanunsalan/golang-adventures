927d25bc73b5a8a89d3a8bdc4f2507d20a525ba2
Furkan Ünsalan <me@furkanunsalan.dev>
2025-04-01 16:13:35 +0300

    day-3 exercise

--- course-content/section-2-go-essentials/profit-calculator/profit_calculator.go
+++ course-content/section-2-go-essentials/profit-calculator/profit_calculator.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
 
 // GOALS
 // 1) Validate user input
@@ -14,15 +18,38 @@ func main() {
 	// var expenses float64
 	// var taxRate float64
 
-	revenue := getUserInput("Revenue: ")
-	expenses := getUserInput("Expenses: ")
-	taxRate := getUserInput("Tax Rate: ")
+	revenue, err1 := getUserInput("Revenue: ")
+
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+
+	expenses, err2 := getUserInput("Expenses: ")
+
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+
+	taxRate, err3 := getUserInput("Tax Rate: ")
+
+	if err1 != nil || err2 != nil || err3 != nil {
+		fmt.Println(err1)
+		return
+	}
 
 	ebt, profit, ratio := calculateFinancials(revenue, expenses, taxRate)
 
 	fmt.Printf("Before Tax: %.2f\n", ebt)
 	fmt.Printf("After Tax (Profit Amount): %.2f\n", profit)
 	fmt.Printf("Profit Ratio: %.2f\n", ratio)
+	storeResults(ebt, profit, ratio)
+}
+
+func storeResults(ebt, profit, ratio float64) {
+	results := fmt.Sprintf("EBT: %.1f\nProfit: %.1f\nRatio: %.3f\n", ebt, profit, ratio)
+	os.WriteFile("results.txt", []byte(results), 0644)
 }
 
 func calculateFinancials(revenue, expenses, taxRate float64) (float64, float64, float64) {
@@ -33,10 +60,14 @@ func calculateFinancials(revenue, expenses, taxRate float64) (float64, float64, 
 	return ebt, profit, ratio
 }
 
-func getUserInput(infoText string) float64 {
+func getUserInput(infoText string) (float64, error) {
 	var userInput float64
 	fmt.Print(infoText)
 	fmt.Scan(&userInput)
 
-	return userInput
+	if userInput <= 0 {
+		return 0, errors.New("value must be positive number")
+	}
+
+	return userInput, nil
 }
